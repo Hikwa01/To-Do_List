fc8b64fa056120f60f2ed01eecae1ed884477147
hikwa01 <hikwajung@gmail.com>
2022-06-19 12:56:24 +0700

    Updates

---
+++ precache-manifest.51f2e259e61ee4f79a3a0156bd9ff0a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aea3e03b8445a87efe2e",
+    "url": "/To-Do_List/static/css/main.07ddae7c.chunk.css"
+  },
+  {
+    "revision": "aea3e03b8445a87efe2e",
+    "url": "/To-Do_List/static/js/main.aea3e03b.chunk.js"
+  },
+  {
+    "revision": "2782a717d958b3b8bcb6",
+    "url": "/To-Do_List/static/js/1.2782a717.chunk.js"
+  },
+  {
+    "revision": "29b989d0f1ad220c7f1e",
+    "url": "/To-Do_List/static/js/runtime~main.29b989d0.js"
+  },
+  {
+    "revision": "552374e98d7784db9453fb1437bced3e",
+    "url": "/To-Do_List/index.html"
+  }
+];
